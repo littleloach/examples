dfd4e4237077754269ef8d3a947fa37511b9209a
youxia <littleloach@gmail.com>
2014-03-04 09:49:52 +0800

    RCP示例程序的最小可运行框架，其它的功能都作为另外的plugin实现

---
+++ com.xkland.examples.rcp.base/src/com/xkland/examples/rcp/base/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.xkland.examples.rcp.base;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.xkland.examples.rcp.base.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
